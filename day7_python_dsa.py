8d6c64d49b77ae8b85b20c69dae5a0b3a68cae00
maseeraruksanashaik-tech <maseeraruksanashaik@gmail.com>
2026-09-18 23:38:58 +0530

    Create day7_python_dsa.py

---
+++ day7_python_dsa.py
@@ -0,0 +1,32 @@
+# Example of set
+names = {"shaik", "sara", "saba", "rehana"}
+print(names)
+
+# Example of automatically removing duplicate values
+ages = {2, 4, 5, 2, 5, 6, 2, 4, 5, 1, 2, 5, 6}
+print(ages)
+
+# Example of add()
+marks = {23, 52, 53, 16, 63, 34}
+marks.add(12)
+print(marks)
+
+# Example of remove()
+names = {"shaik", "sara", "taleem", "muskan"}
+names.remove("shaik")
+print(names)
+
+# Example of discard()
+marks = {289, 468, 476, 468, 561}
+marks.discard(476)
+print(marks)
+
+# Example of in
+ages = {13, 18, 42, 49, 20}
+print(13 in ages)
+print(21 in ages)
+
+# Another example
+names = {"summiiya", "tabasum", "hira", "mahi", "roshani"}
+print("asiya" in names)
+print("hira" in names)
